811f3959a34062e19196ef3b78c34cf5e3fd5ed7
Joseph <josephrock225@gmail.com>
2022-10-29 00:42:13 -0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 
 use std::collections::HashMap;
+use indicatif::ProgressBar;
 
 #[macro_use]
 extern crate lazy_static;
 
 lazy_static! {
-    static ref ATOMIC_LENGTH: HashMap<i32, i32> = {
-        return HashMap::from([
+    static ref ATOMIC_LENGTH: HashMap<i32, i32> =
+        HashMap::from([
             (0  , 0),
             (1  , 3),
             (2  , 3),
@@ -36,10 +37,9 @@ lazy_static! {
             (80 , 6),
             (90 , 6)
         ]);
-    };
 
-    static ref MAGNITUDE: HashMap<i32, i32> = {
-        return HashMap::from([
+    static ref MAGNITUDE: HashMap<i32, i32> = 
+        HashMap::from([
             (0 , 7), // hundred
             (1 , 8), // thousand
             (2 , 7), // million
@@ -47,30 +47,17 @@ lazy_static! {
             (4 , 8), // trillion
             (5 , 11) // quadrillion
         ]);
-    };
 
     static ref PRELOAD: HashMap<i32, i32> = {
         let mut map = HashMap::new();
         for i in 0..1000 {
-            map.insert(i, total_group(i));
+            map.insert(i, preload_sum(i));
         }
         return map;
     };
 }
 
-fn num_groups(num: i32) -> Vec<i32> {
-    let mut groups: Vec<i32> = vec![];
-    let mut curr = num;
-
-    while curr != 0 {
-        groups.push(curr % 1000);
-        curr = curr / 1000;
-    }
-    
-    return groups;
-}
-
-fn total_group(n: i32) -> i32 {
+fn preload_sum(n: i32) -> i32 {
     let mut total: i32 = 0;
 
     let num_hundreds: i32 = n / 100;
@@ -88,38 +75,44 @@ fn total_group(n: i32) -> i32 {
     return total;
 }
 
-fn is_num(n: i32) -> i32 {
-    let groups: Vec<i32> = num_groups(n);
+fn is_num(num: i32) -> i32 {
+    let mut curr = num;
     let mut total = 0;
     let mut i = 0;
 
-    for group in groups {
+    while curr != 0 {
+        let group = curr % 1000;
         total += PRELOAD[&group];
-
         if i > 0 && group > 0 {
             total += MAGNITUDE[&i];
         }
-        i += 1;
-    }
 
+        i += 1;
+        curr = curr / 1000;
+    }
+    
     return total;
 }
 
 fn cosmic_chain(num: i32) {
     let mut curr = num;
     let mut next = is_num(curr);
+    let mut text = format!("num={curr}\n");
 
     while curr != 4 {
-        println!("{curr} is {next}");
+        text.push_str(&format!("{curr} is {next}\n"));
         curr = next;
         next = is_num(curr);
     }
-    println!("4 is cosmic");
-    println!("----")
+    text.push_str("4 is cosmic\n----");
+    println!("{text}");
 }
 
 fn main() {
-    for i in 1..1000001 {
+    let pb = ProgressBar::new(1000000);
+    for i in 1..1_000_001 {
         cosmic_chain(i);
+        pb.inc(1);
     }
+    pb.finish_with_message("done!")
 }
