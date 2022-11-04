6d8d3ad9593285bc9645fa48371ccb720fb71d91
Joseph <josephrock225@gmail.com>
2022-11-04 14:28:50 -0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use once_cell::sync::Lazy;
 use indicatif::{ProgressIterator, ProgressStyle};
 
 
-static ATOMIC_LENGTH: Lazy<HashMap<i32, i32>> = Lazy::new(|| {
+static ATOMIC_LENGTH: Lazy<HashMap<u32, u32>> = Lazy::new(|| {
     HashMap::from([
         (0  , 0),
         (1  , 3),
@@ -38,7 +38,7 @@ static ATOMIC_LENGTH: Lazy<HashMap<i32, i32>> = Lazy::new(|| {
     ])
 });
 
-static MAGNITUDE: Lazy<HashMap<i32, i32>> = Lazy::new(|| {
+static MAGNITUDE: Lazy<HashMap<u32, u32>> = Lazy::new(|| {
     HashMap::from([
         (0 , 7), // hundred
         (1 , 8), // thousand
@@ -49,7 +49,7 @@ static MAGNITUDE: Lazy<HashMap<i32, i32>> = Lazy::new(|| {
     ])
 });
 
-static PRELOAD: Lazy<HashMap<i32, i32>> = Lazy::new(|| {
+static PRELOAD: Lazy<HashMap<u32, u32>> = Lazy::new(|| {
     let mut map = HashMap::with_capacity(1000);
     for i in 0..1000 {
         map.insert(i, preload_sum(i));
@@ -57,7 +57,7 @@ static PRELOAD: Lazy<HashMap<i32, i32>> = Lazy::new(|| {
     return map;
 });
 
-fn preload_sum(n: i32) -> i32 {
+fn preload_sum(n: u32) -> u32 {
     let mut total = 0;
 
     let num_hundreds = n / 100;
@@ -75,7 +75,7 @@ fn preload_sum(n: i32) -> i32 {
     return total;
 }
 
-fn is_num(num: i32) -> i32 {
+fn is_num(num: u32) -> u32 {
     let mut curr = num;
     let mut total = 0;
     let mut i = 0;
@@ -94,15 +94,14 @@ fn is_num(num: i32) -> i32 {
     return total;
 }
 
-fn cosmic_chain(num: i32) -> String {
+fn cosmic_chain(num: u32) -> String {
     let mut curr = num;
-    let mut next = is_num(curr);
     let mut text = format!("num={curr}\n");
 
     while curr != 4 {
+        let next = is_num(curr);
         text.push_str(&format!("{curr} is {next}\n"));
         curr = next;
-        next = is_num(curr);
     }
     text.push_str("4 is cosmic\n----\n");
     return text;
